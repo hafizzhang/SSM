5e03c3876aae141e28e039f553a6cfbc3fed08c9
hafiz.zhang <hafiz.zhang@thebeastshop.com>
2016-09-08 20:57:54 +0800

    import the project

---
+++ src/main/java/com/hafiz/www/service/UserService.java
@@ -0,0 +1,19 @@
+package com.hafiz.www.service;
+
+import com.hafiz.www.po.UserEntity;
+
+import java.util.List;
+
+/**
+ * Desc:用户表相关的service接口
+ * Created by hafiz.zhang on 2016/8/27.
+ */
+public interface UserService {
+
+    /**
+     * 获取所有的用户信息
+     *
+     * @return
+     */
+    List<UserEntity> getAllUsers();
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<head>
+    <title>欢迎页</title>
+</head>
+<body>
+    <div>Hello World</div>
+</body>
+</html>
