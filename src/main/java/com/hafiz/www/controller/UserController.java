1a2fdd229dfcc4ae582e25eb731d12a98dfebff2
hafiz.zhang <hafiz.zhang@thebeastshop.com>
2016-12-11 23:22:17 +0800

    项目搭建和维护

--- src/main/java/com/hafiz/www/controller/UserController.java
+++ src/main/java/com/hafiz/www/controller/UserController.java
@@ -2,12 +2,15 @@ package com.hafiz.www.controller;
 
 import com.hafiz.www.po.UserEntity;
 import com.hafiz.www.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,13 +21,20 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     private UserService userService;
 
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     @ResponseBody
     public List<UserEntity> getAllUsers(){
-        List<UserEntity> list = userService.getAllUsers();
+        logger.info("[GET] /user/all getAllUsers");
+        //List<UserEntity> list = userService.getAllUsers();
+        List<UserEntity> list = new ArrayList<>();
+        logger.debug("This is log of level of debug");
+        logger.trace("log4j2 Demo");
+        logger.error("哎呀，出错啦~");
         return list;
     }
 }
